20864ce8ee56e8ecf8298b64c04aed5b4fead454
Seun Oshitade <seun.oshitade@gmail.com>
2022-03-22 15:31:43 +0000

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,16 +11,17 @@ let tweetData =  {
       "handle": "@SirIsaac"
     },
   "content": {
-      "text": "I am just testing this out!"
+      "text": "If I have seen further it is by standing on the shoulders of Giants!"
     },
   "created_at": 1461116232227
 }
 
-const createTweetElement = function ({ user, content, created_at }){
+const createTweetElement = function (tweet){
+  const { user, content, created_at } = tweet;
   return `<article class="tweet">
   <header class="tweet-header">
     <div>
-      <img id="isaac" src=${user.avatars} alt="image of sir isaac newton">
+      <img id="isaac" src=${user.avatars} alt="image of tweeter account owner">
       <p>
         ${user.name}
       </p> 
@@ -31,7 +32,7 @@ const createTweetElement = function ({ user, content, created_at }){
   </p> 
   <footer>
     <p>
-      ${timeago.format(created_at)}
+      ${created_at}
     </p>
     <div>
       <i class="fa-solid fa-flag"></i>
@@ -41,7 +42,46 @@ const createTweetElement = function ({ user, content, created_at }){
   </footer>
 </article>`;
 }
+// ${timeago.format(created_at)} Replace created at.
 
-const $tweet = createTweetElement(tweetData);
-console.log($tweet);
-$('.the-tweets').append($tweet)
+// const $tweet = createTweetElement(tweetData);
+// console.log($tweet);
+// $('.the-tweets').append($tweet)
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  for(const tweet of tweets){
+    let $tweet = createTweetElement(tweet);
+    console.log($tweet);
+    $('.the-tweets').append($tweet)
+  }
+}
+
+
+
+renderTweets(data);
